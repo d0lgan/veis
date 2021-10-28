c288520b7af78efc9e551c20b31227355feeb800
Dolgan Andiev <andievdolgan08@gmail.com>
2021-10-28 18:57:08 +0300

    add google tag manager and meta
    Former-commit-id: 0bf6523036da68508832f21725657315c4666d6e [formerly 82f1a9bdfc792debd2e21dd50cf48db0574eba7a]
    Former-commit-id: addc650b1feca02082ae97d4075f1bc21c009600

--- resources/views/layouts/site/layout_with_long_header.blade.php
+++ resources/views/layouts/site/layout_with_long_header.blade.php
@@ -20,6 +20,12 @@
 
     @php $locale = $__env->yieldContent('locale') @endphp
 
+    @if($locale == 'ru')
+        <link rel="alternate" href="{{ route('setlocale', ['lang' => 'uk']) }}" hreflang="ua">
+    @elseif($locale == 'uk')
+        <link rel="alternate" href="{{ route('setlocale', ['lang' => 'ru']) }}" hreflang="ru">
+    @endif
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
@@ -30,6 +36,16 @@
 </head>
 
 <body>
+<!-- Google Tag Manager -->
+
+<script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PWXDM7L');</script>
+<!-- End Google Tag Manager -->
+
 <div id="app">
 
 <div class="header">
--- resources/views/layouts/site/layout_with_short_header.blade.php
+++ resources/views/layouts/site/layout_with_short_header.blade.php
@@ -30,6 +30,16 @@
 </head>
 
 <body>
+<!-- Google Tag Manager -->
+
+<script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PWXDM7L');</script>
+<!-- End Google Tag Manager -->
+
 <div id="app">
 
 <div class="header">
@@ -368,9 +378,6 @@
 
 <script src="{{ asset('js/app.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/front/js/front.js') }}"></script>
-
-
-
 
 </body>
 
